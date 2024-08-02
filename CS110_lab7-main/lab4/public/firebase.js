ae885a9002649d49e1f0cb12b6ea1edb622ed745
Rachel Danover <60625627+RDanover@users.noreply.github.com>
2024-08-02 10:21:07 -0700

    Removed Revoked API Key

--- CS110_lab7-main/lab4/public/firebase.js
+++ CS110_lab7-main/lab4/public/firebase.js
@@ -2,11 +2,11 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDuLk2gvX8nV_M8fdTuRPnuQv2n8HZ7dc",
-  authDomain: "example-aa537.firebaseapp.com",
-  projectId: "example-aa537",
-  messagingSenderId: "125740441376",
-  appId: "1:125740441376:web:812d39321fb0262e7ac145"
+  apiKey: "apiKey here",
+  authDomain: "authDomain here",
+  projectId: "projectId here",
+  messagingSenderId: "messagingSenderId here",
+  appId: "appId here"
 };
 
 const app = initializeApp(firebaseConfig);
